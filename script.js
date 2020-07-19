13386192f3cbefc9ff5b63cc877f7755c2ac77ac
Matt Hunt <wmatthunt@gmail.com>
2020-07-19 12:55:46 -0400

    breaking up into mult funcs

--- script.js
+++ script.js
@@ -4,33 +4,46 @@ var answerArray = ["1", "2", "3"]
 var score = 0;
 var answered = false;
 var seconds_left = 60
+var answer = document.getElementById("answer").value;
 
 function jsQuiz()
-{
-    quiz_question()
-    final_score()
-}
-function quiz_question()
 {
     document.getElementById("start_section").style.display = "none"
     document.getElementById("quiz_section").style.display = "block"
     document.getElementById("score_section").style.display = "none"  
+    quiz_question()
+    //final_score()
+}
+function quiz_question()
+{ 
     for (var i = 0; i < questionArray.length; i++)
     {
         document.getElementById("question").innerHTML = questionArray[i];
-        var answer = document.getElementById("answer").value;
-        if (answer === answerArray[i])
-        {
-            console.log("correct")
-            score ++;
-        }
-        else
-        {
-            console.log("wrong")
-            //seconds_left --;
-        }
+    }
+//check answer function
+function check_answer()
+{
+    if (answer === answerArray[i])
+    {
+        correct_answer()
+    }
+    else
+    {
+        wrong_answer()
     }
 }
+//correct answer function
+function correct_answer()
+{
+    console.log("correct")
+    score ++;
+}
+//wrong answer function
+function wrong_answer()
+{
+    console.log("wrong")
+}
+//final score function
 function final_score()
 {
     document.getElementById("start_section").style.display = "none"
