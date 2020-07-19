809cde128eafe8a90559670ca47045f897e464a8
Matt Hunt <wmatthunt@gmail.com>
2020-07-19 16:10:47 -0400

    added all questions

--- README.md
+++ README.md
@@ -4,8 +4,68 @@ A Javascript Fundamentals Quiz
 
 A timed quiz to test knowledge of Javascript and whether and compare scores against other players
 
-Questions and Options:
-Answers:
+Quiz (Question, answers, and then answer)
+"Question 1: Variables are the ____ of programming?"
+A: Nouns, B: Verbs, C: Adjectives , or D :Adverbs 
+A: Nouns
+Question 2: What are variables made up of?
+A: names and constants:, B: titles and values, C: names and values, or D: cats and dogs: 
+C: Names and Values
+Question 3: How to you comfort a Javascript bug?"
+A: Squish it , B: Kill it, C: sing to it, or D: Console it.
+D: Console it
+Question 4: What is the first value in an array?
+A: 1, B: A, C: 0, or D: Alpha"
+C: 0
+Question 5: How do you figure out how many elements are in an array?
+A: .length , B: .width, C: . height, D: .radius
+A: .length
+Question 6: What animal is used for debugging?
+A: Megatron, Rubber Duck, A Tickle Me Elmo, Rubber Chicken
+B: Rubber Duck
+Question 7: Which returns a number from 1 to 10?
+A: Math.Random , B: Math.floor(Math.random() * 10); , C: Math.floor(Math.random() * 10 +1); , or D: Math.floor(Math.random() * 11);
+C: Math.floor(Math.random() * 10 +1);
+Question 8: Which calls the defined function: function myFunction(){}
+A: myFunction, B: function(), C: MyFunction(), or D: myFunction() "
+D: myFunction()
+Question 9: Why do we use functions?
+A: They're cool, B: To prevent repetitive code, C: We like math, or D: None of the above
+B: To prevent repetitive code
+Question 10: What seperates an object's properties and values?
+A: ., B: =, C: :, or D: ;
+C: :
+Question 11: How do you call an objects property?
+A: object.property, B: object, C: property, or D: object-property
+A; object.property
+Question 12: What goes around a string?"
+A: Nothing, B: Parenthesis , C: A Period  , or D: Quotation Marks
+D: Quotation Marks
+Question 13: Does else have a conditional?"
+A: No, B: Yes, C: Maybe, or D: Of course"
+A: No
+Question 14: What goes between an if and an else?
+A: if else, B: else, C: else if, or D: if
+C: else if
+Question 15: Which symbol means and?"
+"A: and, B: &, C: &&&, or D: &&"
+D: &&
+Question 16: Which operator means increment?"
+A: +, B: -+, C: ++ , or D: +="
+C: ++
+Question 17: Which isn't a pop-up box?
+A: alert, B: prompt , C: confirm, or D: yes/no
+D: yes/no
+Question 18: Which variable is most commonly used in a for loop?
+A: x, B: i, C: y, or D: h
+B: i
+Question 19: Which is used to check the content of a string in an if statement?
+A:= , B:== , C:=== , or D: ====
+C: ===
+Question 20: What should you never forget to use?
+A: ;, B: :, C: ,, or D: .
+A: ;
+
 
 Sound Effects are from:
 https://www.myinstants.com/instant/correct-ding/
--- script.js
+++ script.js
@@ -1,9 +1,53 @@
 //var time = document.getElementById("time");
 //question array
-var questionArray = ["Question 1", "Question 2", "Question 3", "Question 4"]
+var questionArray = 
+[
+    "Question 1: Variables are the ____ of programming?", 
+    "Question 2: What are variables made up of?",
+    "Question 3: How to you comfort a Javascript bug?", 
+    "Question 4: What is the first value in an array?",
+    "Question 5: How do you figure out how many elements are in an array?",
+    "Question 6: What animal is used for debugging?",
+    "Question 7: Which returns a number from 1 to 10?",
+    "Question 8: Which calls the defined function: function myFunction(){}",
+    "Question 9: Why do we use functions?",
+    "Question 10: What seperates an object's properties and values?",
+    "Question 11: How do you call an objects property?",
+    "Question 12: What goes around a string?",
+    "Question 13: Does else have a conditional?",
+    "Question 14: What goes between an if and an else?",
+    "Question 15: Which symbol means and?",
+    "Question 16: Which operator means increment?",
+    "Question 17: Which isn't a pop-up box?",
+    "Question 18: Which variable is most commonly used in a for loop?",
+    "Question 19: Which is used to check the content of a string in an if statement?",
+    "Question 20: What should you never forget to use?",
+]
 //answer array
-var optionArray = ["A, B, C, or D", "A, B, C, or D", "A, B, C, or D", "A, B, C, or D",]
-var answerArray = [1, 2, 1, 4]
+var optionArray = 
+[   
+    "A: Nouns, B: Verbs, C: Adjectives , or D :Adverbs", 
+    "A: names and constants:, B: titles and values, C: names and values, or D: cats and dogs",
+    "A: Squish it , B: Kill it, C: sing to it, or D: Console it.", 
+    "A: 1, B: A, C: 0, or D: Alpha",
+    "A: .length , B: .width, C: . height, D: .radius",
+    "A: Megatron, Rubber Duck, A Tickle Me Elmo, Rubber Chicken",
+    "A: Math.Random , B: Math.floor(Math.random() * 10); , C: Math.floor(Math.random() * 10 +1); , or D: Math.floor(Math.random() * 11);",
+    "A: myFunction, B: function(), C: MyFunction(), or D: myFunction() ",
+    "A: They're cool, B: To prevent repetitive code, C: We like math, or D: None of the above",
+    "A: ., B: =, C: :, or D: ;",
+    "A: object.property, B: object, C: property, or D: object-property",
+    "A: Nothing, B: Parenthesis , C: A Period  , or D: Quotation Marks",
+    "A: No, B: Yes, C: Maybe, or D: Of course",
+    "A: if else, B: else, C: else if, or D: if",
+    "A: and, B: &, C: &&&, or D: &&",
+    "A: +, B: -+, C: ++ , or D: +=",
+    "A: alert, B: prompt , C: confirm, or D: yes/no",
+    "A: x, B: i, C: y, or D: h",
+    "A:= , B:== , C:=== , or D: ====",
+    "A: ;, B: :, C: ,, or D: .",
+]
+var answerArray = [1, 3, 4, 3, 1, 2, 3, 4, 2, 3, 1, 4, 1, 3, 4, 3, 4, 2, 3, 1]
 var score = 0;
 var answered = false;
 var seconds_left = 60
