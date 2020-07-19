fda1cc5a9e2563ddfc8a0809aa6e3d47fa0311e6
Matt Hunt <wmatthunt@gmail.com>
2020-07-19 10:57:17 -0400

    working on getting question to post

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
 //question array
 var questionArray = ["1", "2"]
 var answerArray = ["1", "2"]
-var question = Math.floor((Math.random() * questionArray.length))
 var score = 0;
 var answered = 0;
-var time = 60
+var seconds_left = 60
+//var time = document.querySelector(".time");
+//var question = document.querySelector(".question");
 
+// function setTime() {
+//     var timerInterval = setInterval(function() {
+//       seconds_left--;
+//       time.textContent = seconds_left + " seconds left.";
+  
+//       if(seconds_left === 0) {
+//         clearInterval(timerInterval);
+//         sendMessage();
+//       }
+  
+//     }, 1000);
+//   }
+document.getElementById("question") = score;
 function jsQuiz()
 {
-    while (answered < questionArray.length && time != 0)
+    // setTime()
+    document.getElementById("start_section").style.display = "none"
+    document.getElementById("quiz_section").style.display = "block"
+    document.getElementById("score_section").style.display = "none"  
+    while (answered < questionArray.length && seconds_left > 0)
     {
         for (var i = 0; i < questionArray.length; i++)
         {
-            alert (questionArray[i])
+            question = questionArray[i];
             var answer = prompt("what is the answer?")
             if (answer === answerArray[i])
             {
                 alert ("correct")
-                score += 1;
-                answered += 1;
+                score ++;
+                answered ++;
             }
             else
             {
                 alert ("wrong")
-                answered += 1;
+                answered ++;
+                seconds_left --;
             }
         }
     }
@@ -31,5 +50,8 @@ function jsQuiz()
 }
 function final_score()
 {
+    document.getElementById("start_section").style.display = "block"
+    document.getElementById("quiz_section").style.display = "none"
+    document.getElementById("score_section").style.display = "block"
     alert(score)
 }
