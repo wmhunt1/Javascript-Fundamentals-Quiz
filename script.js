dd4508282bd948a680a905e77d808febcb7c085f
Matt Hunt <wmatthunt@gmail.com>
2020-07-19 14:40:41 -0400

    added option array

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
-//document.getElementById("question").innerHTML = "";
+
 //question array
-var questionArray = ["Question 1", "Question 2"]
-var answerArray = [1, 2]
+var questionArray = ["Question 1", "Question 2", "Question 3", "Question 4"]
+//answer array
+var optionArray = ["A, B, C, or D", "A, B, C, or D", "A, B, C, or D", "A, B, C, or D",]
+var answerArray = [1, 2, 1, 4]
 var score = 0;
 var answered = false;
 var seconds_left = 60
 var current_question = 0;
 
+//timer function
+function timer()
+{
+    document.getElementById("time").innerHTML = seconds_left
+}
 //quiz function
 function jsQuiz()
 {
+    
+    timer()
     document.getElementById("start_section").style.display = "none"
     document.getElementById("quiz_section").style.display = "block"
     document.getElementById("score_section").style.display = "none"  
     quiz_question()
     //final_score()
 }
+//question function
 function quiz_question()
 { 
     document.getElementById("question").innerHTML = questionArray[current_question];
+    document.getElementById("option").innerHTML = optionArray[current_question];
 }
 //check answer function
+//sees if value from check answer button = value in answerArray
 function check_answer(x)
 {
     if (x === answerArray[current_question])
@@ -38,13 +50,17 @@ function correct_answer()
 {
     console.log("correct")
     score ++;
+    console.log(score)
+    //moves to next question
     current_question ++;
 }
 //wrong answer function
 function wrong_answer()
 {
     console.log("wrong")
+    console.log(score)
     current_question ++;
+    seconds_left --;
 }
 //final score function
 function final_score()
