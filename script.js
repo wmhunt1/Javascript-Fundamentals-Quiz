5846bd7b038c86dc6b1f202d1a998c5521b738a1
Matt Hunt <wmatthunt@gmail.com>
2020-07-19 10:26:01 -0400

    will go through question array and check answer

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
 //question array
-var questionArray = []
-var answerArray = []
+var questionArray = ["1", "2"]
+var answerArray = ["1", "2"]
 var question = Math.floor((Math.random() * questionArray.length))
+var score = 0;
+var answered = 0;
+var time = 60
+
+function jsQuiz()
+{
+    while (answered < questionArray.length && time != 0)
+    {
+        for (var i = 0; i < questionArray.length; i++)
+        {
+            alert (questionArray[i])
+            var answer = prompt("what is the answer?")
+            if (answer === answerArray[i])
+            {
+                alert ("correct")
+                score += 1;
+                answered += 1;
+            }
+            else
+            {
+                alert ("wrong")
+                answered += 1;
+            }
+        }
+    }
+    final_score()
+}
+function final_score()
+{
+    alert(score)
+}
