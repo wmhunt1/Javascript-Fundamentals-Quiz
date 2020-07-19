2829f276bb60169938c9e6e95880c8700136e8f2
Matt Hunt <wmatthunt@gmail.com>
2020-07-19 11:27:36 -0400

    now posts question

--- script.js
+++ script.js
@@ -1,49 +1,30 @@
 //question array
-var questionArray = ["1", "2"]
-var answerArray = ["1", "2"]
+var questionArray = ["Question 1", "Question 2", "Question 3"]
+var answerArray = ["1", "2", "3"]
 var score = 0;
 var answered = 0;
 var seconds_left = 60
-//var time = document.querySelector(".time");
-//var question = document.querySelector(".question");
-
-// function setTime() {
-//     var timerInterval = setInterval(function() {
-//       seconds_left--;
-//       time.textContent = seconds_left + " seconds left.";
-  
-//       if(seconds_left === 0) {
-//         clearInterval(timerInterval);
-//         sendMessage();
-//       }
-  
-//     }, 1000);
-//   }
-document.getElementById("question") = score;
+var question = 0
 function jsQuiz()
 {
-    // setTime()
     document.getElementById("start_section").style.display = "none"
     document.getElementById("quiz_section").style.display = "block"
     document.getElementById("score_section").style.display = "none"  
-    while (answered < questionArray.length && seconds_left > 0)
+    for (var i = 0; i < questionArray.length; i++)
     {
-        for (var i = 0; i < questionArray.length; i++)
+        document.getElementById("question").innerHTML = questionArray[i];
+        var answer = prompt("what is the answer?")
+        if (answer === answerArray[i])
         {
-            question = questionArray[i];
-            var answer = prompt("what is the answer?")
-            if (answer === answerArray[i])
-            {
-                alert ("correct")
-                score ++;
-                answered ++;
-            }
-            else
-            {
-                alert ("wrong")
-                answered ++;
-                seconds_left --;
-            }
+            alert ("correct")
+            score ++;
+            answered ++;
+        }
+        else
+        {
+            alert ("wrong")
+            answered ++;
+            seconds_left --;
         }
     }
     final_score()
@@ -53,5 +34,5 @@ function final_score()
     document.getElementById("start_section").style.display = "block"
     document.getElementById("quiz_section").style.display = "none"
     document.getElementById("score_section").style.display = "block"
-    alert(score)
+    document.getElementById("final_score").innerHTML = score;
 }
