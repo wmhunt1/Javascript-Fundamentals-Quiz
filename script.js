632b5f1fb8398b90979ce5441b67ebaa0072376c
Matt Hunt <wmatthunt@gmail.com>
2020-07-19 11:36:28 -0400

    to final score w/o any qs

--- script.js
+++ script.js
@@ -13,25 +13,29 @@ function jsQuiz()
     for (var i = 0; i < questionArray.length; i++)
     {
         document.getElementById("question").innerHTML = questionArray[i];
-        var answer = prompt("what is the answer?")
+        var answer = document.getElementById("answer").value;
         if (answer === answerArray[i])
         {
-            alert ("correct")
+            console.log("correct")
             score ++;
             answered ++;
         }
         else
         {
-            alert ("wrong")
+            console.log("wrong")
             answered ++;
             seconds_left --;
         }
     }
     final_score()
 }
+function submit()
+{
+
+}
 function final_score()
 {
-    document.getElementById("start_section").style.display = "block"
+    document.getElementById("start_section").style.display = "none"
     document.getElementById("quiz_section").style.display = "none"
     document.getElementById("score_section").style.display = "block"
     document.getElementById("final_score").innerHTML = score;
